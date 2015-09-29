df8cfcd163f77a9cc94911d02e663b88e1396804
Trenton Adam Hyer <thyer@palmhearts.cs.byu.edu>
2015-09-29 16:57:05 -0600

    added intersect checking code

--- bzagents/PathAgent.py
+++ bzagents/PathAgent.py
@@ -13,6 +13,7 @@ class PathAgent(object):
         self.bzrc = bzrc
         self.commands = []
         self.points = []
+        self.edges = []
         self.visibilityGraph = None
         self.index = index
         self.counter = 0
@@ -35,10 +36,13 @@ class PathAgent(object):
                 self.points.append((flag.x, flag.y))
                 break
                 
-        # append obstacle points
+        # append obstacle points, edges
         for obstacle in self.bzrc.get_obstacles():
+            i = 0
             for point in obstacle:
                 self.points.append(point)
+                self.edges.append([point, obstacle[(i+1)%len(obstacle)]])
+                i += 1
 
         length = len(self.points)
         self.visibilityGraph = [[-1 for x in range(length)] for x in range(length)]
@@ -53,10 +57,53 @@ class PathAgent(object):
                     else:
                         self.visibilityGraph[row][col] = 0
 
-        print self.visibilityGraph
+        self.print_visibility_graph();
 
     def is_visible(self, point1, point2):
+        # check if they share an edge
+        for edge in self.edges:
+            if point1 in edge:
+                if point2 in edge:
+                    return True
+        
+        # if there are no intersecting line segments
+        for edge in self.edges:
+            if line_segments_intersect([point1, point2], edge):
+                return False
+        
+        # no edges intersected
         return True
+        
+    def line_segments_intersect(edge1, edge2):
+        x = 0
+        y = 1
+        
+        # crossproduct 1, 2: check points in edge 1 are on opposite sides of edge 2
+        e = edge2[0]
+        f = edge2[1]
+        p1 = edge1[0]
+        p2 = edge1[1]
+        
+        cp1 = (f[x]-e[x])(p1[y]-f[y]) - (f[y] - e[y])(p1[x] - f[x])
+        cp2 = (f[x]-e[x])(p2[y]-f[y]) - (f[y] - e[y])(p2[x] - f[x])
+        if (cp1 > 0 and cp2 > 0) or (cp1 < 0 and cp2 < 0):
+            return False
+        
+        # crossproduct 3, 4: check points in edge 2 are on opposite sides of edge 1
+        e = edge1[0]
+        f = edge1[1]
+        p1 = edge2[0]
+        p2 = edge2[1]
+        
+        cp1 = (f[x]-e[x])(p1[y]-f[y]) - (f[y] - e[y])(p1[x] - f[x])
+        cp2 = (f[x]-e[x])(p2[y]-f[y]) - (f[y] - e[y])(p2[x] - f[x])
+        if (cp1 > 0 and cp2 > 0) or (cp1 < 0 and cp2 < 0):
+            return False        
+        return True
+        
+    def print_visibility_graph(self):
+        for row in self.visibility_graph:
+            print row
 
 def main():
     # Process CLI arguments.
