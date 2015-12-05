f7a4c6d5a864b2388b0c10c70783d83bde9a8948
Trenton Adam Hyer <thyer@deepdish.cs.byu.edu>
2015-12-05 13:49:37 -0700

    did math

--- bzagents/KalmanFilterAgent.py
+++ bzagents/KalmanFilterAgent.py
@@ -14,28 +14,72 @@ class KalmanFilterAgent(object):
         self.bzrc = bzrc
         self.tank_index = tank_index
         self.commands = []
-        self.ticks = 0
-        self.mu_t = NP.matrix('0.0 0 0 0 0 0')
+        
+        # constant matrices
         self.sigma_x = NP.matrix('0.1 0 0 0 0 0;' + \
         '0 0.1 0 0 0 0; 0 0 10 0 0 0; 0 0 0 0.1 0 0;' + \
         '0 0 0 0 0.1 0; 0 0 0 0 0 10')
         self.sigma_z = NP.matrix('25.0 0; 0 25')
+        self.H = NP.matrix('1.0 0 0 0 0 0; 0 0 0 1 0 0')
+        
+        # instantiated matrices
+        self.mu_t = NP.matrix('0.0; 0; 0; 0; 0; 0')
         self.sigma_t = NP.matrix('100.0 0 0 0 0 0;' + \
         '0 0.1 0 0 0 0; 0 0 0.1 0 0 0; 0 0 0 100 0 0;' + \
         '0 0 0 0 0.1 0; 0 0 0 0 0 0.1')
-        self.H = NP.matrix('1.0 0 0 0 0 0; 0 0 0 1 0 0')
-        self.F = NP.matrix('0.0 0.0 0.0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0;' + \
-        '0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0')
+        
+        # variable matrix
+        self.F = NP.matrix('1.0 0.0 0.0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0;' + \
+        '0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1')
         c = .01
-        self.F[2, 1] = c
-        self.F[5, 4] = c
+        self.F[2, 1] = -c
+        self.F[5, 4] = -c
+        
+        # other tracking variables
+        self.delta_time = 0
 
     def tick(self):
-        self.ticks += 1
         self.commands = []
+        # print "tick"
+        
+        # update our F matrix depending on how much time has passed
+        d_time = 1 # TODO: figure out how to determine the time passed
+        if d_time != self.delta_time:
+            self.update_f_matrix(d_time)
+            selfdelta_time = d_time
+            
+        # get our updated location estimate and standard deviation
+        enemy_tank = self.bzrc.get_othertanks()[0]
+        x, y = enemy_tank.x, enemy_tank.y
+        z_current = NP.matrix([[x], [y]])
+        self.update_position_estimate(z_current)
+        
         return
 
+    def update_position_estimate(self, z_current):
+        f_sigma_ft = self.F * self.sigma_t * self.F.T
+        kalman_gain = self.calc_kalman_gain(f_sigma_ft)
+        mu_current = self.F * self.mu_t + kalman_gain * (z_current - self.H * self.F * self.mu_t)
+        sigma_current = self.calculate_sigma_current(f_sigma_ft, kalman_gain)
+        print "sigma_current: " + str(sigma_current)
 
+    def calc_kalman_gain(self, f_sigma_ft):
+        top = f_sigma_ft * self.H.T
+        bottom = self.H * (f_sigma_ft + self.sigma_x) * self.H.T + self.sigma_z
+        kalman_gain = top * bottom.I
+        return kalman_gain
+    def update_f_matrix(self, delta_time):
+        self.F[0,1] = delta_time
+        self.F[0,2] = (delta_time ** 2) / 2
+        self.F[1,2] = delta_time
+        self.F[3,4] = delta_time
+        self.F[3,5] = (delta_time ** 2) / 2
+        self.F[4,5] = delta_time
+
+    def calculate_sigma_current(self, f_sigma_ft, kalman_gain):
+        left = NP.identity(6) - kalman_gain * self.H
+        right = f_sigma_ft + self.sigma_x
+        return left * right
 #####################
 ### MAIN FUNCTION ###
 #####################
